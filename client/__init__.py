82a1e1a7e45d08ce9e289ade349b6fe550e8a0a1
PlopeanuOana <oanaplopeanu11@gmail.com>
2021-11-20 17:09:27 +0200

    added login modal

--- client/__init__.py
+++ client/__init__.py
@@ -9,3 +9,11 @@ blueprint = Blueprint('client', __name__)
 @blueprint.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@blueprint.route('/generic', methods=['GET'])
+def generic():
+    return render_template('generic.html')
+
+@blueprint.route('/elements', methods=['GET'])
+def elements():
+    return render_template('elements.html')
