283341384ed8684d78a35172cff9ed2b2e407375
Ana-Maria Dragos <anadragos98@gmail.com>
2021-11-20 18:35:49 +0200

    add endpoints

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, session
+from flask_session import Session
 from model import db
+from rest import Blueprints
 
 import client
 
 class FlaskWrapper(Flask):
     def init(self):
+        for bp in Blueprints:
+            self.register_blueprint(bp[1], url_prefix=bp[0])
+
         # Register template endpoints
         self.register_blueprint(client.blueprint)
 
         self.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
         self.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        self.config['SECRET_KEY'] = 'secret'
         self.config['UPLOAD_FOLDER'] = 'invoices'
         self.config['ASSETS_FOLDER'] = os.path.join('templates', 'assets')
         self.config['IMAGES_FOLDER'] = os.path.join('templates', 'images')
+        self.config['SESSION_TYPE'] = 'filesystem'
+        self.config['SECRET_KEY'] = 'secret'
+        self.secret_key = 'secret'
 
+        sess = Session()
+        sess.init_app(self)
 
     def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
         if not self.debug or os.getenv('WERKZEUG_RUN_MAIN') == 'true':
