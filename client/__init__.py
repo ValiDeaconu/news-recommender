f9cdddcd9e4c2f1f68db8f996c216c8099e23a66
Valentin Deaconu <valideaconu98@gmail.com>
2021-11-20 18:24:25 +0200

    Link SignUp

--- client/__init__.py
+++ client/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, render_template, request
 
+from form.register import Form as RegisterForm
+from model import User
+
+from passlib.hash import sha256_crypt
+
 blueprint = Blueprint('client', __name__)
 
 @blueprint.route('/', methods=['GET'])
@@ -13,3 +18,22 @@ def generic():
 @blueprint.route('/elements', methods=['GET'])
 def elements():
     return render_template('elements.html')
+
+@blueprint.route('/signup', methods=['GET', 'POST'])
+def signup():
+    rf = RegisterForm()
+
+    if request.method == 'POST':
+        username = rf.username.data
+        password = rf.password.data
+        confirm_password = rf.confirm_password.data
+
+        if rf.validate_on_submit():
+            # if password != confirm_password:
+
+            User(username = username, password = sha256_crypt.hash(password), mutation_rate = 0.0) # .save()
+            return render_template('signup.html', form=rf, success=True, message='Contul a fost creat cu succes.')
+            
+        return render_template('signup.html', form=rf, success=False, message='Error.')
+
+    return render_template('signup.html', form=rf)
--- form/register.py
+++ form/register.py
@@ -1,7 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms.fields.simple import TextField
+from wtforms import StringField
+from wtforms.widgets import PasswordInput
+from wtforms.validators import DataRequired, Length, EqualTo
+
+FIELD_REQUIRED_TPL = 'Campul "{}" este obligatoriu.'
+MINIMUM_LENGTH_TPL = 'Campul "{}" trebuie sa contina minim {} caractere.'
 
 class Form(FlaskForm):
-    username = TextField('username')
-    password = TextField('password')
-    confirm_password = TextField('confirm_password')
+    username = StringField('username', validators=[
+        DataRequired(message=FIELD_REQUIRED_TPL.format('nume de utilizator')), 
+        Length(min=6, message=MINIMUM_LENGTH_TPL.format('nume de utilizator', '6'))
+    ])
+    password = StringField('password', widget=PasswordInput(hide_value=False), validators=[
+        DataRequired(message=FIELD_REQUIRED_TPL.format('parola')), 
+        Length(min=8, message=MINIMUM_LENGTH_TPL.format('parola', '8'))
+    ])
+    confirm_password = StringField('confirm_password', widget=PasswordInput(hide_value=False), validators=[
+        DataRequired(message=FIELD_REQUIRED_TPL.format('parola')), 
+        Length(min=8, message=MINIMUM_LENGTH_TPL.format('parola', '8')),
+        EqualTo('password', message='Parolele nu coincid.')
+    ])
